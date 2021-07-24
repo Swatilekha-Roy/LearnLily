ca6c8f41462dcfcd7bd87a95ef7e234acee2c3b3
Swatilekha Roy <swatilekha.roy4@gmial.com>
2021-07-24 12:39:41 +0530

    Log in system added

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ $('#return-to-top').click(function() {
         scrollTop : 0                       
     }, 500);
 });
+
+
+/* Logout functionality */
+window.logout = function() {
+
+	// TELLS OUR SERVER TO LOG THE USER OUT
+	fetch('/~/LearnLily/logout', { method: 'POST'});
+	
+	// TELLS OUR BROWSER TO SHOW A MESSAGE TO OUR USER
+	alert('Logged Out!');
+	
+	// REDIRECTS THE USER TO THE HOMEPAGE
+	location.href = '/~/LearnLily/landpage'
+}
